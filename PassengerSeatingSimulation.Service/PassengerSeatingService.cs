e0fcdd9e64cab4884423859b48ddec4f1ffe156c
aravind20 <raj.pandian@gmail.com>
2021-07-06 18:05:02 +0100

    Add files via upload

---
+++ PassengerSeatingSimulation.Service/PassengerSeatingService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using PassengerSeatingSimulation.Domain.Dto;
+using PassengerSeatingSimulation.Service.Interfaces;
+
+namespace PassengerSeatingSimulation.Service
+{
+    public class PassengerSeatingService : IPassengerSeatingService
+    {
+        /// <summary>
+        /// returns last passengers status
+        /// </summary>
+        /// <param name="passengers"></param>
+        /// <returns></returns>
+        public bool  IsLastPassengerGotOwnSeat(List<Passenger> passengers)
+        {
+            var lastPassenger = passengers.Last();
+            return lastPassenger.allocatedSeatNumber == lastPassenger.occupiedSeatNumber;
+        }
+    }
+}
