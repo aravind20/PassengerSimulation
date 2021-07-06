d6b680b75661d43838a1bcd8fbf3c8b2bdb7720e
aravind20 <raj.pandian@gmail.com>
2021-07-06 16:09:14 +0100

    Add files via upload

---
+++ PassengerSeatingSimulation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Autofac;
+using PassengerSeatingSimulation.Service;
+using PassengerSeatingSimulation.Service.Interfaces;
+
+namespace PassengerSeatingSimulation
+{
+    class Program
+    {
+        private static IContainer CompositionRoot()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Application>();
+            builder.RegisterType<PassengerSeatingService>().As<IPassengerSeatingService>();
+            return builder.Build();
+        }
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+             await CompositionRoot().Resolve<Application>().Run();
+
+        }
+    }
+}
